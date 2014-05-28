37427fd336c0069f6cd238da22b25d28e206e149
Ryan <maturski@gmail.com>
2014-05-28 10:42:21 -0400

    Initial Commit

---
+++ Git Test/RMViewController.m
@@ -0,0 +1,29 @@
+//
+//  RMViewController.m
+//  Git Test
+//
+//  Created by Ryan Maturski on 5/28/14.
+//  Copyright (c) 2014 Ryan Maturski. All rights reserved.
+//
+
+#import "RMViewController.h"
+
+@interface RMViewController ()
+
+@end
+
+@implementation RMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Git Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by Ryan Maturski on 5/28/14.
+//  Copyright (c) 2014 Ryan Maturski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RMAppDelegate class]));
+    }
+}
