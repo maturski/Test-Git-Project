2d4e5e3aa84a66de5d445f5ae095dfbfb53b873e
Ryan <maturski@gmail.com>
2014-05-28 11:05:24 -0400

    Added a new class

---
+++ Git Test/RMTest.h
@@ -0,0 +1,13 @@
+//
+//  RMTest.h
+//  Git Test
+//
+//  Created by Ryan Maturski on 5/28/14.
+//  Copyright (c) 2014 Ryan Maturski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RMTest : NSObject
+
+@end
---
+++ Git Test/RMTest.m
@@ -0,0 +1,13 @@
+//
+//  RMTest.m
+//  Git Test
+//
+//  Created by Ryan Maturski on 5/28/14.
+//  Copyright (c) 2014 Ryan Maturski. All rights reserved.
+//
+
+#import "RMTest.h"
+
+@implementation RMTest
+
+@end
